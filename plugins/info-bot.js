c144acfc6d1e20ef3dad60a64ef5fa2f78eb018b
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-21 13:33:01 -0400

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `👸🏼 ¡Hola! Soy 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩, en que puedo ayudarte hoy?\n\n🌸 Usa *.menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, ` ¡Hola! Soy ⟢◦ 𝐍𝐅 𝐁𝐎𝐓, en que puedo ayudarte hoy?\n\n🌸 Usa *.menu* para ver mis comandos.`, m, rcanal, )
 }
   
 if (/^.bermuda/i.test(m.text)) {
